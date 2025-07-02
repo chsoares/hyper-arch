0a106acd3619ec080cce668fd9496d4daea76f73
chsoares <cordial.road4051@fastmail.com>
2025-07-02 18:07:04 -0300

    Whatever

---
+++ fish/aliases.fish
@@ -0,0 +1,12 @@
+# General
+alias whatsapp elecwhat
+alias zen zen-browser
+alias editor gnome-text-editor
+
+abbr --add yay 'yay -Sy'
+abbr --add install 'yay -Sy'
+abbr --add update 'yay -Syu --noconfirm'
+
+abbr --add gs "git status"
+abbr --add ga "git add ."
+abbr --add gc "git commit -m"
