734e19950b373d79c1b04bec0eb53316bdff9a0f
chsoares <cordial.road4051@fastmail.com>
2025-07-03 16:12:19 -0300

    added aliases

--- config/.config/fish/aliases.fish
+++ config/.config/fish/aliases.fish
@@ -1,4 +1,5 @@
 # General
+## Aliases
 alias whatsapp elecwhat
 alias zen zen-browser
 alias editor gnome-text-editor
@@ -6,6 +7,7 @@ alias editor gnome-text-editor
 alias ls 'eza --icons'
 alias tree 'eza --icons --tree'
 
+## Abbreviations
 abbr --add yay 'yay -Sy'
 abbr --add install 'yay -Sy'
 abbr --add update 'yay -Syu --noconfirm'
@@ -13,3 +15,19 @@ abbr --add update 'yay -Syu --noconfirm'
 abbr --add gs "git status"
 abbr --add ga "git add ."
 abbr --add gc "git commit -m"
+
+
+# CTFs and stuff
+## Aliases
+
+## Abbreviations
+abbr --add nmap 'sudo nmap'
+abbr --add openvpn 'sudo openvpn'
+
+## Variables
+set -x rockyou '/usr/share/seclists/Passwords/Leaked-Databases/rockyou.txt'
+
+## Executables
+
+
+
