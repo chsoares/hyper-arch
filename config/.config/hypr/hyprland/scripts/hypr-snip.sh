5beb3727efa0fc89190eb2afd44005bbc49f0cd3
chsoares <cordial.road4051@fastmail.com>
2025-08-12 23:02:46 -0300

    third time is the charm

--- config/.config/hypr/hyprland/scripts/hypr-snip.sh
+++ config/.config/hypr/hyprland/scripts/hypr-snip.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env fish
 
+# Carrega as variáveis do ambiente se existir
+if test -f ~/Lab/env.fish
+    source ~/Lab/env.fish
+end
+
 # Gera o nome do arquivo
 set filename (date "+%Y%m%d-%H:%M:%S").png
 
 # Decide o output
-if test -d $boxpwd
+if test -n "$boxpwd" -a -d "$boxpwd"
+    # Cria a pasta screenshots se não existir
+    mkdir -p "$boxpwd/screenshots"
     set output_filename "$boxpwd/screenshots/satty-$filename"
 else
-    set output_filename "$HOME/Pictures/Screenshots"
+    # Cria a pasta Screenshots se não existir
+    mkdir -p "$HOME/Pictures/Screenshots"
+    set output_filename "$HOME/Pictures/Screenshots/satty-$filename"
 end
 
 grimblast --freeze save area - | satty \
